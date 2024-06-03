03c704bce08151affdb0a663419b8cea23fa12c1
Renan Martins Conceição Silva de Lima <renanmartinslima@id.uff.br>
2024-06-03 14:57:35 -0300

    Commit geral
    Comitando todos os arquivos que não houveram alteração

---
+++ evasim/config.py
@@ -0,0 +1,10 @@
+# Smart Bulb Adress
+SMART_BULB_IP_ADRESS = '192.168.1.105' # 
+SMART_BULB_PORT      = 55443
+
+
+# MQTT Broker configuration
+# Note: For the FRED robot, the broker must be at IP 192.168.1.100
+MQTT_BROKER_ADRESS   = '192.168.1.100' # Raspberry Pi address
+MQTT_PORT            = 1883
+EVA_TOPIC_BASE       = 'EVA'
